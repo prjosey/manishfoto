62c6d294073918180029ddd2785e4554577c0e78
Priyanka Joshi <priyanka.josey@gmail.com>
2016-05-01 17:08:56 -0400

    about us

--- js/main.js
+++ js/main.js
@@ -10,3 +10,31 @@
     },  3000);
   });
 
+function MM_preloadImages() { //v1.2
+  if (document.images) {
+    var imgFiles = MM_preloadImages.arguments;
+    var preloadArray = new Array();
+    for (var i=0; i<imgFiles.length; i++) {
+      preloadArray[i] = new Image;
+      preloadArray[i].src = imgFiles[i];
+    }
+  }
+}
+
+function MM_swapImage() { //v1.0
+  var i,theObj,j=0,swapArray=new Array,oldArray=document.MM_swapImgData;
+  for (i=0; i < (MM_swapImage.arguments.length-2); i+=3) {
+    theObj = eval(MM_swapImage.arguments[(navigator.appName == 'Netscape')?i:i+1])
+    if (theObj != null) {
+      swapArray[j++] = theObj;
+      swapArray[j++] = (oldArray==null || oldArray[j-1]!=theObj)?theObj.src:oldArray[j];
+      theObj.src = MM_swapImage.arguments[i+2];
+  } }
+  document.MM_swapImgData = swapArray; //used for restore
+}
+
+function MM_swapImgRestore() { //v1.0
+  if (document.MM_swapImgData != null)
+    for (var i=0; i<(document.MM_swapImgData.length-1); i+=2)
+      document.MM_swapImgData[i].src = document.MM_swapImgData[i+1];
+}
